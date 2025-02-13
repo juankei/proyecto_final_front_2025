f40793fd476a44d76012815b687198a470f12bcc
juankei <juancamilocosf@gmail.com>
2025-02-13 10:37:36 +0100

    .

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule,DOCUMENT } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
 import { AuthService } from '@auth0/auth0-angular';
 import { IonContent, IonHeader, IonToolbar, IonTitle,
   IonList, IonIcon, IonMenu, IonLabel, IonRouterOutlet,
